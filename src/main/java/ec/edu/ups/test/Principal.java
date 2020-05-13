dbca731be9775eefc2ed0865c96a53571e2dbae6
Romel_Avila <NANCY@DESKTOP-1JG1RL0>
2020-05-13 16:18:05 -0500

    actualizacion codigo

--- src/main/java/ec/edu/ups/test/Principal.java
+++ src/main/java/ec/edu/ups/test/Principal.java
@@ -5,7 +5,11 @@
  */
 package ec.edu.ups.test;
 
+import ec.edu.ups.controlador.ControladorTelefono;
 import ec.edu.ups.controlador.ControladorUsuario;
+import ec.edu.ups.dao.TelefonoDAO;
+import ec.edu.ups.dao.UsuarioDAO;
+import ec.edu.ups.vista.VistaTelefono;
 import ec.edu.ups.vista.VistaUsuario;
 import java.util.Scanner;
 
@@ -18,7 +22,11 @@ public class Principal {
     {
         Scanner teclado= new Scanner (System.in);
         VistaUsuario vista = new VistaUsuario();
-        ControladorUsuario controlador = new ControladorUsuario(vista);
+        VistaTelefono vistaT = new VistaTelefono();
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        TelefonoDAO telefonoDAO = new TelefonoDAO();
+        ControladorUsuario controladorUsuario = new ControladorUsuario(vista, vistaT, usuarioDAO, telefonoDAO);
+        ControladorTelefono controladorTelefono = new ControladorTelefono(vistaT, telefonoDAO);
         
         boolean Salir= false;
         
@@ -35,23 +43,24 @@ public class Principal {
             {
                 case 1:
                     System.out.println("---Registrar Usuario---");
-                    controlador.registrar();
+                    controladorUsuario.registrar();
+                    System.out.println("Usuarios Registrados");
+                    controladorUsuario.verUsuarios();
                     break;
                     
                 case 2:
+                    System.out.println("---Iniciar Secion---");
+                    
                     boolean salir= false;
                     while(salir== false)
                     {
-                        System.out.println("---Iniciar Secion---");
-                        
-                        
                         System.out.println("--menu--");
                         System.out.println(" 1. Registrar ");
                         System.out.println(" 2. Modificar ");
                         System.out.println(" 3. Eliminar ");
                         System.out.println(" 4. Buscar ");
-                        System.out.println(" 6. Listar sus telefonos ");
-                        System.out.println(" 7. Salir ");
+                        System.out.println(" 5. Listar sus telefonos ");
+                        System.out.println(" 6. Salir ");
                         
                         int respuesta2= teclado.nextInt();
                         
@@ -59,7 +68,32 @@ public class Principal {
                         {
                             case 1:
                                 System.out.println("---Registrar telefono---");
-                                
+                                controladorTelefono.registrar();
+                                controladorUsuario.agregarTelefono();
+                                controladorUsuario.verUsuario();
+                                break;
+                            case 2:
+                                System.out.println("---Modificar telefono---");
+                                controladorTelefono.actualizar();
+                                controladorUsuario.actualizarTelefono();
+                                controladorUsuario.verUsuario();
+                                break;
+                            case 3:
+                                System.out.println("---Eliminar telefono---");
+                                controladorTelefono.eliminar();
+                                controladorUsuario.eleiminarTelefono();
+                                controladorUsuario.verUsuario();
+                                break;
+                            case 4:
+                                controladorTelefono.eliminar();
+                                controladorUsuario.buscarTelefono();
+                                controladorUsuario.verUsuario();
+                                break;
+                            case 5:
+                                controladorTelefono.verTelefonos();
+                                break;
+                            case 6:
+                                salir= true;
                                 break;
                         }
                     }
