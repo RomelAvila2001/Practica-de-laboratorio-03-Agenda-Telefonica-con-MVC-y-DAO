bc67323b9d5bd4e4e22d4e036540adf3a67b9948
Romel_Avila <NANCY@DESKTOP-1JG1RL0>
2020-05-12 22:34:13 -0500

    Actualisacion codigo

--- src/main/java/ec/edu/ups/vista/VistaUsuario.java
+++ src/main/java/ec/edu/ups/vista/VistaUsuario.java
@@ -5,7 +5,9 @@
  */
 package ec.edu.ups.vista;
 
+
 import ec.edu.ups.modelo.Usuario;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -33,4 +35,53 @@ public class VistaUsuario {
         
         return new Usuario(cedula,nombre,apellido,correo,contraseña);
     }
+    public Usuario iniciarSesion()
+    {
+        System.out.println("Ingrese Correo");
+        String correo=teclado.next();
+        System.out.println("Ingrese contraseña");
+        String contraseña= teclado.next();
+        
+        return new Usuario(null, null, null, correo, contraseña);
+    }
+    
+    
+    
+    public Usuario actualizarUsuario() {
+        System.out.println("Ingresa la cedula del cliente a actualizar");
+        String cedula=teclado.next();
+        System.out.println("Ingrese los nuevos Datos de usuario");
+        System.out.println("Ingrese nombre");
+        String nombre= teclado.next();
+        System.out.println("Ingrese apellido");
+        String apellido= teclado.next();
+        System.out.println("Ingrese correo electronico");
+        String correo= teclado.next();
+        System.out.println("Igrese contraseña");
+        String contraseña= teclado.next();
+        return new Usuario(cedula, nombre, apellido,correo,contraseña);
+    }
+
+    public Usuario eliminarUsuario() {
+        System.out.println("Ingresa la cedula del cliente a eliminar");
+        String cedula = teclado.next();
+        return new Usuario(cedula,null,null,null,null);
+    }
+
+    public String buscarUsuario() {
+        System.out.println("Ingresa la cedula del cliente a buscar");
+        String cedula=teclado.next();
+        return cedula;
+    }
+
+    public void verUsuario(Usuario usuario) {
+        System.out.println("Datos del Cliente: " + usuario);
+    }
+
+    public void verUsuarios(List<Usuario> usuarios) {
+        for (Usuario usuario : usuarios) {
+            System.out.println("Datos del Cliente: " + usuario);
+        }
+    }
+    
 }
