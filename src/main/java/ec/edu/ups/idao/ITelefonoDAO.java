3e3d41c18c75885dce94e7e6883bfe4b01539fce
Romel_Avila <NANCY@DESKTOP-1JG1RL0>
2020-05-14 02:43:26 -0500

    actualizacion del codigo

--- src/main/java/ec/edu/ups/idao/ITelefonoDAO.java
+++ src/main/java/ec/edu/ups/idao/ITelefonoDAO.java
@@ -6,7 +6,7 @@
 package ec.edu.ups.idao;
 
 import ec.edu.ups.modelo.Telefono;
-import java.util.List;
+import java.util.Collection;
 
 /**
  *
@@ -15,8 +15,8 @@ import java.util.List;
 public interface ITelefonoDAO {
     //Métodos CRUD
     public void create(Telefono telefono);
-    public Telefono read(int codigo);
+    public Telefono read(String codigo);
     public void update(Telefono telefono);
     public void delete(Telefono telefono);   
-    public List<Telefono> findAll();
+    public Collection<Telefono> findAll();
 }
